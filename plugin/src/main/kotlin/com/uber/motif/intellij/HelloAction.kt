dccb773c2fca2144a2ea9014aac5ddbc48136309
Leland Takamine <leland@uber.com>
2018-07-21 12:14:37 -0700

    Index Scope files

--- plugin/src/main/kotlin/com/uber/motif/intellij/HelloAction.kt
+++ plugin/src/main/kotlin/com/uber/motif/intellij/HelloAction.kt
@@ -2,17 +2,11 @@ package com.uber.motif.intellij
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.util.indexing.FileBasedIndex
 import com.uber.motif.intellij.index.ScopeIndex
 
 class HelloAction : AnAction("Hello") {
 
     override fun actionPerformed(event: AnActionEvent) {
-        val project = event.project!!
-        FileBasedIndex.getInstance().getFilesWithKey(ScopeIndex.ID, setOf("LELAND"), { file ->
-            println(file)
-            true
-        }, GlobalSearchScope.allScope(project))
+        println(ScopeIndex.getScopeFiles(event.project!!))
     }
 }
--- plugin/src/main/kotlin/com/uber/motif/intellij/index/ScopeIndex.kt
+++ plugin/src/main/kotlin/com/uber/motif/intellij/index/ScopeIndex.kt
@@ -1,21 +1,30 @@
 package com.uber.motif.intellij.index
 
 import com.intellij.ide.highlighter.JavaFileType
-import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.UserDataHolderBase
+import com.intellij.lang.LighterAST
+import com.intellij.lang.LighterASTNode
+import com.intellij.lang.LighterASTTokenNode
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiFile
+import com.intellij.psi.JavaTokenType
 import com.intellij.psi.impl.source.JavaFileElementType
+import com.intellij.psi.impl.source.tree.JavaElementType
+import com.intellij.psi.impl.source.tree.LightTreeUtil
+import com.intellij.psi.search.ProjectScopeBuilder
+import com.intellij.psi.tree.IElementType
 import com.intellij.util.indexing.*
-import com.intellij.util.io.DataExternalizer
-import com.intellij.util.io.EnumeratorStringDescriptor
-import java.io.DataInput
-import java.io.DataOutput
+import com.intellij.util.io.BooleanDataDescriptor
+import java.util.*
 
-class ScopeIndex : FileBasedIndexExtension<String, String>() {
+class ScopeIndex : ScalarIndexExtension<Boolean>() {
 
-    override fun getIndexer() = DataIndexer<String, String, FileContent> { fileContent ->
-        mutableMapOf<String, String>()
+    override fun getIndexer() = DataIndexer<Boolean, Void?, FileContent> { fileContent ->
+        val tree: LighterAST = (fileContent as FileContentImpl).lighterASTForPsiDependentIndex
+        if (ScopeDetector(tree).hasScope()) {
+            mapOf(true to null)
+        } else {
+            mapOf()
+        }
     }
 
     override fun getInputFilter() = object : DefaultFileTypeSpecificInputFilter(JavaFileType.INSTANCE) {
@@ -27,22 +36,41 @@ class ScopeIndex : FileBasedIndexExtension<String, String>() {
         }
     }
 
-    override fun getValueExternalizer() = object : DataExternalizer<String> {
-        override fun save(out: DataOutput, value: String) {
-            out.writeUTF(value)
+    override fun getVersion() = Random().nextInt()
+    override fun getName() = ID
+    override fun dependsOnFileContent() = true
+    override fun getKeyDescriptor() = BooleanDataDescriptor.INSTANCE!!
+
+    private class ScopeDetector(private val tree: LighterAST) {
+
+        fun hasScope(): Boolean {
+            val root: LighterASTNode = tree.root
+            val annotationId: LighterASTTokenNode = root.firstChild(JavaElementType.CLASS)
+                    ?.firstChild(JavaElementType.MODIFIER_LIST)
+                    ?.firstChild(JavaElementType.ANNOTATION)
+                    ?.firstChild(JavaElementType.JAVA_CODE_REFERENCE)
+                    ?.firstChild(JavaTokenType.IDENTIFIER) as LighterASTTokenNode? ?: return false
+            val name = tree.charTable.intern(annotationId.text).toString()
+            return name == "Scope"
         }
 
-        override fun read(`in`: DataInput): String {
-            return `in`.readUTF()
+        private fun LighterASTNode.firstChild(type: IElementType): LighterASTNode? {
+            return LightTreeUtil.firstChildOfType(tree, this, type)
         }
     }
 
-    override fun getVersion() = 1
-    override fun getName() = ID
-    override fun dependsOnFileContent() = true
-    override fun getKeyDescriptor() = EnumeratorStringDescriptor()
-
     companion object {
-        val ID: ID<String, String> = com.intellij.util.indexing.ID.create("ScopeIndex")
+
+        private val ID: ID<Boolean, Void> = com.intellij.util.indexing.ID.create("ScopeIndex")
+
+        fun getScopeFiles(project: Project): Set<VirtualFile> {
+            val scopeFiles = mutableSetOf<VirtualFile>()
+            val projectScopeBuilder = ProjectScopeBuilder.getInstance(project)
+            FileBasedIndex.getInstance().getFilesWithKey(ID, setOf(true), {
+                scopeFiles.add(it)
+                true
+            }, projectScopeBuilder.buildProjectScope())
+            return scopeFiles
+        }
     }
 }
