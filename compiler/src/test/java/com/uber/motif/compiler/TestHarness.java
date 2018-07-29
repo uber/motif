580ab5b3d21aa640f3dee408610e2683649ac6c3
Leland Takamine <leland@uber.com>
2018-07-29 13:06:02 -0700

    it

--- compiler/src/test/java/com/uber/motif/compiler/TestHarness.java
+++ compiler/src/test/java/com/uber/motif/compiler/TestHarness.java
@@ -1,13 +1,24 @@
 package com.uber.motif.compiler;
 
+import com.google.testing.compile.Compilation;
+import com.google.testing.compile.JavaFileObjects;
+import dagger.internal.codegen.ComponentProcessor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import javax.tools.JavaFileObject;
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
+
+import static com.google.testing.compile.CompilationSubject.assertThat;
+import static com.google.testing.compile.Compiler.javac;
 
 @RunWith(Parameterized.class)
 public class TestHarness {
@@ -18,7 +29,7 @@ public class TestHarness {
         File[] testCaseDirs = testCaseRoot.listFiles((dir, name) -> dir.isDirectory() && name.startsWith("T"));
         if (testCaseDirs == null) throw new IllegalStateException("Could not find test case directories: " + testCaseRoot);
         return Arrays.stream(testCaseDirs)
-                .map(file -> new Object[]{file, file.getName()})
+                .map(file -> new Object[]{file.getAbsoluteFile(), file.getName()})
                 .collect(Collectors.toList());
     }
 
@@ -30,7 +41,23 @@ public class TestHarness {
     }
 
     @Test
-    public void test() {
-        
+    public void test() throws IOException {
+        JavaFileObject[] files = Files.walk(testCaseDir.toPath())
+                .map(Path::toFile)
+                .filter(file -> !file.isDirectory() && file.getName().endsWith(".java"))
+                .map(file -> {
+                    try {
+                        return JavaFileObjects.forResource(file.toURI().toURL());
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .toArray(JavaFileObject[]::new);
+
+        Compilation compilation = javac().withProcessors(
+                new AnnotationProcessor(),
+                new ComponentProcessor()).compile(files);
+
+        assertThat(compilation).succeeded();
     }
 }
