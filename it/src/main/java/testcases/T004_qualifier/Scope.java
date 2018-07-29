ad0ffe626c8c176c932bbdbf75d257910c5c74ab
Leland Takamine <leland@uber.com>
2018-07-29 15:06:09 -0700

    More tests

---
+++ it/src/main/java/testcases/T004_qualifier/Scope.java
@@ -0,0 +1,26 @@
+package testcases.T004_qualifier;
+
+import javax.inject.Named;
+
+@com.uber.motif.Scope
+public interface Scope {
+
+    String string();
+
+    class Objects {
+
+        String s(@Named("a") String a, @Named("b") String b) {
+            return a + b;
+        }
+
+        @Named("a")
+        String a() {
+            return "a";
+        }
+
+        @Named("b")
+        String b() {
+            return "b";
+        }
+    }
+}
