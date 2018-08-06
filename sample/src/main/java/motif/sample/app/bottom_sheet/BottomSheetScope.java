8b250278baf9ec27be6ef82f987bec8ddc1f949c
Utkarsh <ut@uber.com>
2018-08-06 14:09:10 -0700

    Add Scope to Demonstrate Multiple Parents Navigation

--- sample/src/main/java/motif/sample/app/bottom_sheet/BottomSheetScope.java
+++ sample/src/main/java/motif/sample/app/bottom_sheet/BottomSheetScope.java
@@ -4,6 +4,7 @@ import android.view.ViewGroup;
 
 import motif.Scope;
 import motif.sample.app.bottom_header.BottomHeaderScope;
+import motif.sample.app.photo_grid.PhotoGridScope;
 import motif.sample.app.photo_list.PhotoListScope;
 import motif.sample.lib.controller.ControllerObjects;
 
@@ -15,6 +16,8 @@ public interface BottomSheetScope {
     BottomHeaderScope header(ViewGroup parent);
 
     PhotoListScope photoList(ViewGroup parent);
+
+    PhotoGridScope photoGrid(ViewGroup parent);
 
     @motif.Objects
     abstract class Objects extends ControllerObjects<BottomSheetController, BottomSheetView> {}
