d9778f12ca8a25774938671435280d5ff0a0b17b
Leland Takamine <leland@uber.com>
2018-08-09 13:42:21 -0700

    Add TODO

--- compiler/src/main/java/motif/compiler/errors/parsing/ParsingError.kt
+++ compiler/src/main/java/motif/compiler/errors/parsing/ParsingError.kt
@@ -2,4 +2,8 @@ package motif.compiler.errors.parsing
 
 import javax.lang.model.element.Element
 
+// TODO Make this a sealed class. Wherever we throw a ParsingError, convert to a subclass of this
+// class. For instance, we throw a ParsingError when we find multiple qualifiers on the same method.
+// We should instead throw a more specific subclass "MultipleQualifiersError : ParsingError". We can
+// then add tests for these cases in the same way we test for GraphValidationErrors.
 class ParsingError(val element: Element, override val message: String) : RuntimeException(message)
