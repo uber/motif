e7df239fd08c392fdd4803d4a63649949268d78f
Leland Takamine <leland@uber.com>
2018-08-16 15:07:16 -0700

    Remove unused property

--- ir/src/main/kotlin/motif/ir/graph/errors/GraphValidationError.kt
+++ ir/src/main/kotlin/motif/ir/graph/errors/GraphValidationError.kt
@@ -15,56 +15,20 @@
  */
 package motif.ir.graph.errors
 
-import de.vandermeer.asciitable.AT_Context
-import de.vandermeer.asciitable.AsciiTable
-import de.vandermeer.asciithemes.u8.U8_Grids
-import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment
 import motif.ir.graph.DependencyCycle
 import motif.ir.graph.DuplicateFactoryMethod
 import motif.ir.graph.Node
 import motif.ir.source.base.Dependency
 import motif.ir.source.base.Type
 
-sealed class GraphError {
-    abstract val message: String
-}
+sealed class GraphError
 
-class DependencyCycleError(val cycles: List<DependencyCycle>) : GraphError() {
+class DependencyCycleError(val cycles: List<DependencyCycle>) : GraphError()
 
-    override val message: String = StringBuilder().apply {
-        appendln()
-        appendln("===== DEPENDENCY CYCLE FOUND ====")
-    }.toString()
-}
-
-class DuplicateFactoryMethodsError(val duplicates: List<DuplicateFactoryMethod>) : GraphError() {
-
-    override val message: String = StringBuilder().apply {
-        appendln()
-        appendln("===== DUPLICATE FACTORY METHODS ====")
-    }.toString()
-}
+class DuplicateFactoryMethodsError(val duplicates: List<DuplicateFactoryMethod>) : GraphError()
 
 class MissingDependenciesError(
         val requiredBy: Node,
-        val dependencies: List<Dependency>) : GraphError() {
+        val dependencies: List<Dependency>) : GraphError()
 
-    private val tableContext = AT_Context()
-            .setGrid(U8_Grids.borderStrongDoubleLight())
-
-    override val message: String = AsciiTable(tableContext).apply {
-        addRule()
-        addRow("MISSING DEPENDENCIES").setTextAlignment(TextAlignment.CENTER)
-        dependencies.forEach { dependency ->
-            addRow(dependency).setPaddingLeft(1)
-        }
-    }.render()
-}
-
-class ScopeCycleError(val cycle: List<Type>) : GraphError() {
-
-    override val message: String = StringBuilder().apply {
-        appendln()
-        appendln("===== SCOPE CYCLE FOUND ====")
-    }.toString()
-}
+class ScopeCycleError(val cycle: List<Type>) : GraphError()
