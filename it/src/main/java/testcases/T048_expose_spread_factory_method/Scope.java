30ce9063056b37b8de9c7f8f4250c5692a4fc488
Leland Takamine <leland@uber.com>
2019-04-04 15:30:16 -0700

    Handle @Expose + @Spread correctly. Previously we were auto-exposing all @Spread dependencies. Now correctly expose @Spread dependencies only when annotated with @Expose.

---
+++ it/src/main/java/testcases/T048_expose_spread_factory_method/Scope.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package testcases.T048_expose_spread_factory_method;
+
+import motif.Expose;
+import motif.Spread;
+
+@motif.Scope
+public interface Scope {
+
+    Child child();
+
+    @motif.Objects
+    abstract class Objects {
+
+        @Expose
+        @Spread
+        abstract Spreadable spreadable();
+    }
+
+    @motif.Dependencies
+    interface Dependencies {}
+}
