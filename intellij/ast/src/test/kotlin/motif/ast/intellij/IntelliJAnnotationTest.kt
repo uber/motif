3905396f4b0e0607f36f7338db007517c9eefeb3
Leland Takamine <leland@uber.com>
2019-06-24 18:55:00 -0700

    IntelliJ: Basic Scope hierarchy UI
    * Add MotifPanel UI to display Scope hierarchy in a tree.
    * Use built-in ProjectFileIndex to find Scope classes instead of custom index.
    * Fix IntelliJAnnotation equality and add tests.

---
+++ intellij/ast/src/test/kotlin/motif/ast/intellij/IntelliJAnnotationTest.kt
@@ -0,0 +1,242 @@
+/*
+ * Copyright (c) 2018-2019 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package motif.ast.intellij
+
+import com.intellij.codeInsight.AnnotationUtil
+import com.intellij.openapi.project.Project
+import com.intellij.psi.*
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
+import org.assertj.core.api.Assertions.assertThat
+import org.intellij.lang.annotations.Language
+import javax.inject.Named
+
+class IntelliJAnnotationTest : LightCodeInsightFixtureTestCase() {
+
+    lateinit var psiElementFactory: PsiElementFactory
+
+    override fun setUp() {
+        super.setUp()
+
+        psiElementFactory = PsiElementFactory.SERVICE.getInstance(project)
+    }
+
+    override fun getTestDataPath(): String {
+        return "testData"
+    }
+
+    fun testEqual() {
+        createAnnotationClass("""
+            package test;
+
+            @interface A {}
+        """.trimIndent())
+
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @A class Foo {}
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @A class Bar {}
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isEqualTo(barAnnotation)
+    }
+
+    fun testNotEqual() {
+        createAnnotationClass("""
+            package test;
+
+            @interface A {}
+        """.trimIndent())
+
+        createAnnotationClass("""
+            package test;
+
+            @interface B {}
+        """.trimIndent())
+
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @A class Foo {}
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @B class Bar {}
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isNotEqualTo(barAnnotation)
+    }
+
+    fun testEqual_qualified() {
+        createAnnotationClass("""
+            package test;
+
+            @interface A {}
+        """.trimIndent())
+
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @test.A class Foo {}
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @A class Bar {}
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isEqualTo(barAnnotation)
+    }
+
+    fun testEqual_named() {
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named("a")
+            class Foo {}
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named("a")
+            class Bar {}
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isEqualTo(barAnnotation)
+    }
+
+    fun testNotEqual_named() {
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named("a")
+            class Foo {}
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named("b")
+            class Bar {}
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isNotEqualTo(barAnnotation)
+    }
+
+    fun testEqual_namedReference() {
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named(Foo.S)
+            class Foo {
+              static final String S = "a";
+            }
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named(Bar.S)
+            class Bar {
+              static final String S = "a";
+            }
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isEqualTo(barAnnotation)
+    }
+
+    fun testNotEqual_namedReference() {
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named(Foo.S)
+            class Foo {
+              static final String S = "a";
+            }
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named(Bar.S)
+            class Bar {
+              static final String S = "b";
+            }
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isNotEqualTo(barAnnotation)
+    }
+
+    fun testEqual_namedReferenceAndString() {
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named("a")
+            class Foo {}
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named(Bar.S)
+            class Bar {
+              static final String S = "a";
+            }
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isEqualTo(barAnnotation)
+    }
+
+    fun testNotEqual_namedReferenceAndString() {
+        val fooAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named("a")
+            class Foo {}
+        """.trimIndent())
+
+        val barAnnotation = getClassAnnotation("""
+            package test;
+
+            @javax.inject.Named(Bar.S)
+            class Bar {
+              static final String S = "b";
+            }
+        """.trimIndent())
+
+        assertThat(fooAnnotation).isNotEqualTo(barAnnotation)
+    }
+
+    private fun createAnnotationClass(@Language("JAVA") classText: String): IntelliJClass {
+        val psiClass = myFixture.addClass(classText)
+        return IntelliJClass(project, psiElementFactory.createType(psiClass))
+    }
+
+    private fun getClassAnnotation(@Language("JAVA") classText: String): IntelliJAnnotation {
+        val psiClass = myFixture.addClass(classText)
+        val psiAnnotation = psiClass.annotations.single()
+        return IntelliJAnnotation(project, psiAnnotation)
+    }
+}
---
+++ intellij/src/main/kotlin/motif/intellij/GraphFactory.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2018-2019 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package motif.intellij
+
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElementFactory
+import com.intellij.psi.PsiJavaFile
+import com.intellij.psi.PsiManager
+import motif.Scope
+import motif.ast.IrClass
+import motif.ast.intellij.IntelliJClass
+import motif.core.ResolvedGraph
+import kotlin.system.measureTimeMillis
+
+class GraphFactory(private val project: Project) {
+
+    private val psiManager = PsiManager.getInstance(project)
+    private val psiElementFactory = PsiElementFactory.SERVICE.getInstance(project)
+
+    fun compute(): ResolvedGraph {
+        val scopeClasses = { getScopeClasses() }.runAndLog("Found Scope classes.")
+        return { ResolvedGraph.create(scopeClasses) }.runAndLog("Processed graph.")
+    }
+
+    private fun getScopeClasses(): List<IrClass> {
+        val scopeClasses = mutableListOf<IrClass>()
+        ProjectFileIndex.SERVICE.getInstance(project).iterateContent { file ->
+            scopeClasses.addAll(getScopeClasses(file))
+            true
+        }
+        return scopeClasses
+    }
+
+    private fun getScopeClasses(virtualFile: VirtualFile): List<IrClass> {
+        val psiFile = psiManager.findFile(virtualFile) ?: return emptyList()
+        val javaFile = psiFile as? PsiJavaFile ?: return emptyList()
+        return javaFile.classes
+                .filter(this::isScopeClass)
+                .map(psiElementFactory::createType)
+                .map { type ->
+                    IntelliJClass(project, type)
+                }
+    }
+
+    private fun isScopeClass(psiClass: PsiClass): Boolean {
+        return psiClass.annotations.find { it.qualifiedName == Scope::class.qualifiedName} != null
+    }
+
+    private fun <T : Any> (() -> T).runAndLog(message: String): T {
+        lateinit var result: T
+        val duration = measureTimeMillis { result = this() }
+        log("$message (${duration}ms)")
+        return result
+    }
+
+    private fun log(message: String) {
+        Notifications.Bus.notify(
+                Notification("Motif", "Motif Graph", message, NotificationType.INFORMATION))
+    }
+}
