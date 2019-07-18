e144ad9bb3835bf8d48aba0d824f92e8893683a8
Leland Takamine <leland@uber.com>
2019-07-18 11:16:05 -0700

    Base IntelliJ functionality

---
+++ intellij/src/main/kotlin/motif/intellij/ChildChangeListener.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2018-2019 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package motif.intellij
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiTreeChangeEvent
+import com.intellij.psi.PsiTreeChangeListener
+import com.intellij.psi.PsiWhiteSpace
+
+class ChildChangeListener(private val callback: (PsiElement) -> Unit) : PsiTreeChangeListener {
+
+    override fun beforePropertyChange(event: PsiTreeChangeEvent) {
+        log("beforePropertyChange", event)
+    }
+
+    override fun beforeChildAddition(event: PsiTreeChangeEvent) {
+        log("beforeChildAddition", event)
+    }
+
+    override fun beforeChildReplacement(event: PsiTreeChangeEvent) {
+        log("beforeChildReplacement", event)
+    }
+
+    override fun beforeChildrenChange(event: PsiTreeChangeEvent) {
+        log("beforeChildrenChange", event)
+    }
+
+    override fun beforeChildMovement(event: PsiTreeChangeEvent) {
+        log("beforeChildMovement", event)
+    }
+
+    override fun beforeChildRemoval(event: PsiTreeChangeEvent) {
+        log("beforeChildRemoval", event)
+    }
+
+    override fun childrenChanged(event: PsiTreeChangeEvent) {
+        log("childrenChanged", event)
+    }
+
+    override fun propertyChanged(event: PsiTreeChangeEvent) {
+        log("propertyChanged", event)
+    }
+
+    override fun childReplaced(event: PsiTreeChangeEvent) {
+        log("childReplaced", event)
+        if (event.oldChild is PsiWhiteSpace && event.newChild is PsiWhiteSpace) return
+        callback(event.child)
+    }
+
+    override fun childMoved(event: PsiTreeChangeEvent) {
+        log("childMoved", event)
+        if (event.child is PsiWhiteSpace) return
+        callback(event.child)
+    }
+
+    override fun childRemoved(event: PsiTreeChangeEvent) {
+        log("childRemoved", event)
+        if (event.child is PsiWhiteSpace) return
+        callback(event.child)
+    }
+
+    override fun childAdded(event: PsiTreeChangeEvent) {
+        log("childAdded", event)
+        if (event.child is PsiWhiteSpace) return
+        callback(event.child)
+    }
+
+    private fun log(name: String, event: PsiTreeChangeEvent) {
+//        log("""=======$name=======
+//            |parent: ${event.parent}
+//            |oldParent: ${event.oldParent}
+//            |newParent: ${event.newParent}
+//            |child: ${event.child}
+//            |oldChild: ${event.oldChild}
+//            |newChild: ${event.newChild}
+//            |element: ${event.element}
+//            |propertyName: ${event.propertyName}
+//            |oldValue: ${event.oldValue}
+//            |newValue: ${event.newValue}
+//        """.trimMargin())
+    }
+}
---
+++ intellij/src/main/kotlin/motif/intellij/GraphManager.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2018-2019 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package motif.intellij
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.ProjectComponent
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import com.intellij.openapi.project.Project
+import com.intellij.psi.*
+import com.intellij.psi.util.parents
+import motif.ast.IrClass
+import motif.ast.IrType
+import motif.ast.intellij.IntelliJClass
+import motif.ast.intellij.IntelliJType
+import motif.core.ResolvedGraph
+import motif.models.ConstructorFactoryMethod
+import motif.models.Scope
+
+sealed class GraphState {
+
+    object Uninitialized : GraphState()
+
+    object Loading : GraphState()
+
+    class Error(val graph: ResolvedGraph): GraphState()
+
+    class Valid(project: Project, val graph: ResolvedGraph): GraphState() {
+
+        val invalidator = GraphInvalidator(project, graph)
+    }
+}
+
+class GraphManager(private val project: Project) : ProjectComponent {
+
+    private val graphFactory = GraphFactory(project)
+
+    private val listeners = mutableListOf<Listener>()
+
+    private var graphState: GraphState = GraphState.Uninitialized
+
+    override fun initComponent() {
+
+    }
+
+    override fun disposeComponent() {
+
+    }
+
+    fun refresh() {
+        setGraphState(GraphState.Loading)
+        ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Refresh") {
+
+            override fun run(indicator: ProgressIndicator) {
+                ApplicationManager.getApplication().runReadAction {
+                    val graph = graphFactory.compute()
+                    val state = if (graph.errors.isEmpty()) {
+                        GraphState.Valid(project, graph)
+                    } else {
+                        GraphState.Error(graph)
+                    }
+                    setGraphState(state)
+                }
+            }
+        })
+    }
+
+    fun addListener(listener: Listener) {
+        listeners.add(listener)
+        listener.onStateChange(graphState)
+    }
+
+    fun removeListener(listener: Listener) {
+        listeners.remove(listener)
+    }
+
+    private fun setGraphState(graphState: GraphState) {
+        this.graphState = graphState
+        listeners.forEach { it.onStateChange(graphState) }
+    }
+
+    interface Listener {
+
+        fun onStateChange(state: GraphState)
+    }
+}
+
+class GraphInvalidator(private val project: Project, private val graph: ResolvedGraph) {
+
+    private val psiElementFactory = PsiElementFactory.SERVICE.getInstance(project)
+
+    private val relevantTypes: Set<IrType> by lazy {
+        graph.scopes.flatMap { scope ->
+            (listOfNotNull(scope.objects?.clazz)
+                    + scope.clazz
+                    + spreadClasses(scope)
+                    + constructorClasses(scope))
+                    .flatMap { clazz -> typeAndSupertypes((clazz as IntelliJClass).psiClass) }
+                    .map { IntelliJType(project, psiElementFactory.createType(it)) }
+        }.toSet()
+    }
+
+    fun shouldInvalidate(changedElement: PsiElement): Boolean {
+        return (sequenceOf(changedElement) + changedElement.parents())
+                .mapNotNull { it as? PsiClass }
+                .map { psiElementFactory.createType(it) }
+                .any { IntelliJType(project, it) in relevantTypes }
+    }
+
+    private fun spreadClasses(scope: Scope): List<IrClass> {
+        return scope.factoryMethods.mapNotNull { it.spread }
+                .map { spread -> spread.clazz }
+    }
+
+    private fun constructorClasses(scope: Scope): List<IrClass> {
+        return scope.factoryMethods
+                .filterIsInstance<ConstructorFactoryMethod>()
+                .mapNotNull { it.returnType.type.type.resolveClass() }
+    }
+
+    private fun typeAndSupertypes(psiClass: PsiClass): Set<PsiClass> {
+        if (psiClass.qualifiedName == "java.lang.Object") {
+            return emptySet()
+        }
+        return setOf(psiClass) + psiClass.supers.flatMap { typeAndSupertypes(it) }
+    }
+}
