6782804eb63c3496c1e7842787564d5f550197ad
TonyTang <tonytang@uber.com>
2022-05-07 18:32:42 -0700

    Use theme app compat to load the photo.

--- samples/sample/src/main/java/motif/sample/app/root/RootActivity.java
+++ samples/sample/src/main/java/motif/sample/app/root/RootActivity.java
@@ -15,13 +15,14 @@
  */
 package motif.sample.app.root;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import motif.ScopeFactory;
 
-public class RootActivity extends Activity {
+public class RootActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
