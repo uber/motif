8c5f428f0eca515f954a85140443c230598a3048
James Barr <jbarr@uber.com>
2022-07-05 23:33:22 -0700

    Config updates

--- config/spotless/copyright.kt
+++ config/spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) $YEAR. Uber Technologies, Inc.
+ * Copyright (c) $YEAR Uber Technologies, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
