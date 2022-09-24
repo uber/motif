3dc82c20bf4a29682e81110911b7c0ce0c16f27d
James Barr <jbarr@uber.com>
2022-09-24 01:52:11 -0700

    Add utils to work around KSP swallowing exceptions

---
+++ compiler/ksp/src/main/kotlin/motif/compiler/ksp/MessageWatcher.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package motif.compiler.ksp
+
+import com.google.devtools.ksp.KspErrorThrower
+import motif.compiler.processing.XAnnotation
+import motif.compiler.processing.XAnnotationValue
+import motif.compiler.processing.XElement
+import motif.compiler.processing.XMessager
+import javax.tools.Diagnostic
+
+/**
+ * A message watcher that re-throws exceptions from the KSP devtools package
+ * to work around an issue where exceptions are swallowed.
+ *
+ *  https://github.com/google/ksp/issues/974
+ */
+internal class MessageWatcher : XMessager() {
+  override fun onPrintMessage(
+    kind: Diagnostic.Kind,
+    msg: String,
+    element: XElement?,
+    annotation: XAnnotation?,
+    annotationValue: XAnnotationValue?
+  ) {
+    if (kind == Diagnostic.Kind.ERROR) {
+      KspErrorThrower.rethrowKspError(msg)
+    }
+  }
+}
